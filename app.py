4de5eaad61d9aa4050c51fc8e0a8254c9d276f4e
IT22171092RANDRanasingha <dilushar10@gmail.com>
2024-06-29 12:54:51 +0530

    moniter application

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect
 from helper import preprocessing,vectorizer,get_prediction
 
 app = Flask(__name__)
+print('flask server started')
 
 data = dict()
 reviews = []
@@ -13,14 +14,22 @@ def index():
     data['reviews'] = reviews
     data['positive'] = positive
     data['negative'] = negative
+    print('===========open home page============')
     return render_template('index.html', data=data)
 
 @app.route("/", methods = ['post'])
 def my_post():
     text = request.form['text']
+    print(f'Text : {text}')
+
     preprocessed_txt = preprocessing(text)
+    print(f'preprocessed Text : {preprocessed_txt}')
+
     vectorized_txt = vectorizer(preprocessed_txt)
+    print(f'vectorized Text : {vectorized_txt}')
+
     prediction = get_prediction(vectorized_txt)
+    print(f'prediction Text : {prediction}')
 
     if prediction == 'negative':
         global negative
