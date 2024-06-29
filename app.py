442b76b499d30de1629714bf07abfb17e0b8c8db
IT22171092RANDRanasingha <dilushar10@gmail.com>
2024-06-29 12:42:21 +0530

    cmplt sentiment

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from helper import preprocessing,vectorizer,get_prediction
 
 app = Flask(__name__)
 
 data = dict()
-reviews = ['Good product', 'Bad product', 'I like it']
-positive = 2
-negative = 1
+reviews = []
+positive = 0
+negative = 0
 
 @app.route("/")
 def index():
@@ -14,6 +15,22 @@ def index():
     data['negative'] = negative
     return render_template('index.html', data=data)
 
+@app.route("/", methods = ['post'])
+def my_post():
+    text = request.form['text']
+    preprocessed_txt = preprocessing(text)
+    vectorized_txt = vectorizer(preprocessed_txt)
+    prediction = get_prediction(vectorized_txt)
+
+    if prediction == 'negative':
+        global negative
+        negative += 1
+    else:
+        global positive
+        positive += 1
+
+    reviews.insert(0, text)
+    return redirect(request.url)
 
 
 if __name__ == "__main__":
